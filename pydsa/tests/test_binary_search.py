105d7d79b3715458b2f26da4d62139b06c2c9828
soummyaah <soumya13106@iiitd.ac.in>
2016-04-07 20:38:03 +0530

    Resolving Conflicts

--- pydsa/tests/test_binary_search.py
+++ pydsa/tests/test_binary_search.py
@@ -6,7 +6,13 @@ def test_binary_search():
     item = randint(1, 10)
     a = sorted(a)
     ans = binary_search(a, item)
-    if(ans == -1): 
+    if(ans == -1):
         assert item not in a
-    else: 
+    else:
         assert a[ans] == item
+    ans1 = binary_search(a, item, True)
+    ans2 = binary_search(a, item, True, 0, len(a) - 1)
+    if(ans1 == ans2 == -1):
+        assert item not in a
+    else:
+        assert a[ans1] == item == a[ans2]
