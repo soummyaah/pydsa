1db556b15531e97c1d28893ba8088cfda057c91d
soummyaah <soumya13106@iiitd.ac.in>
2016-05-01 20:38:26 +0530

    Tests modified

--- pydsa/tests/test_linked_list.py
+++ pydsa/tests/test_linked_list.py
@@ -1,8 +1,6 @@
-import logging
-import unittest
 from pydsa import LinkedList
 
-class test_linked_list(unittest.TestCase):
+class test_linked_list():
 
     test_list = LinkedList()
 
@@ -20,15 +18,15 @@ class test_linked_list(unittest.TestCase):
 
     # Test is_empty
     def step1(self):
-        self.assertTrue(self.test_list.is_empty())
+        assert self.test_list.is_empty() == True
 
     # Test insert_at_start
     def step2(self):
         self.test_list.insert_at_start(5)
         # print(self.test_list.toString())
-        self.assertTrue(self.test_list.head.get_value() == 5)
-        self.assertIsNone(self.test_list.head.get_next())
-        self.assertFalse(self.test_list.is_empty())
+        assert self.test_list.head.get_value() == 5
+        assert self.test_list.head.get_next() == None
+        assert self.test_list.is_empty() == False
 
     # Test insert_at_end
     def step3(self):
@@ -36,25 +34,25 @@ class test_linked_list(unittest.TestCase):
         curr = self.test_list.head
         while curr.get_next():
             curr = curr.get_next()
-        self.assertEqual(curr.get_value(), 6)
-        self.assertIsNone(curr.get_next())
+        assert curr.get_value() == 6
+        assert curr.get_next() == None
 
     # Test find
     def step4(self):
         self.test_list.insert_at_start(4)
-        self.assertEqual(self.test_list.head, self.test_list.find(4))
+        assert self.test_list.head == self.test_list.find(4)
 
     # Test delete
     def step5(self):
-        self.assertEqual(self.test_list.find(4), self.test_list.head)
+        assert self.test_list.find(4) == self.test_list.head
         self.test_list.delete(4)
         self.assertRaises(ValueError, self.test_list.find, 4)
 
     # Test length function
     def step6(self):
-        self.assertEqual(len(self.test_list), 2)
+        assert len(self.test_list) == 2
         self.test_list.insert_at_start(3)
-        self.assertEqual(len(self.test_list), 3)
+        assert len(self.test_list) == 3
         self.test_list.delete(6)
-        self.assertEqual(len(self.test_list), 2)
+        assert len(self.test_list) == 2
         # def toString(self):
